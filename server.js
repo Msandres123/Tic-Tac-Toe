768c132e35680cff19a67e888199dae5782a9f85
Mikael Andres <mikaelandres@Mikaels-MacBook-Pro.local>
2021-05-03 13:18:16 -0400

    work please

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
 const express = require('express')
-const fetch = require('isomorphic-unfetch')
 const path = require('path') 
 
 const app = express()
 
 const port = process.env.PORT || 5000
 
-app.use(express.static('./client/public')) 
+app.use(express.static('public')) 
 
 app.get('*', (req, res) => {
-    res.sendFile(path.resolve('./index.html'))
+    res.sendFile(path.resolve('./public/index.html'))
 })
 
 app.listen(port, () => {
